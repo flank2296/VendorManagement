de44d8576e4a9ec4db92d7dcc1521c6e20d2a63c
AnkushGochke <ankushgochke@gmail.com>
2023-12-03 18:11:27 +0530

    [models] Added core models needed for vendor management

---
+++ core/models/vendor.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Vendor(models.Model):
+    """Model which holds vendor's meta"""
+
+    name = models.CharField(max_length=255)
+    contact_details = models.TextField()
+    address = models.TextField()
+    vendor_code = models.CharField(max_length=50, unique=True)
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"]),
+            models.Index(fields=["vendor_code"]),
+        ]
+        db_table = "Vendor"
